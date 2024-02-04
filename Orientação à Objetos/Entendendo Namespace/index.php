5f115eb43edfa9c54a1041130997e403d4cf5806
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-02-04 09:47:51 -0300

    .

---
+++ Orientação à Objetos/Entendendo Namespace/index.php
@@ -0,0 +1,8 @@
+<?php
+require 'classes1.php';
+require 'classes2.php';
+
+$a = new classe1\MinhaClasse(); 
+echo $a->testar();
+
+?>
---
+++ Orientação à Objetos/Entendendo a PSR/2)/index.php
@@ -0,0 +1,11 @@
+<?php
+//PSR é uma melhor maneira de programar//
+
+//<?= é a mesma coisa de <?php
+//Nunca declarar (Classe, Function, constants, etc) e implementar (gerar o arquvio, iniciar um prompt) em um mesmo arquvio 
+//Classes deve iniciar com o nome em Maiusculo (Ex. class MatematicaBasica)
+//Constante precisa ser tudo em letra Maiuscula (Ex. const DATE_VERSION = '...')
+//Nomes de métodos devem ser declarados com camelCase (Ex. public function listarTudo())
+
+
+?>
