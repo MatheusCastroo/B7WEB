caa1f7a8527ad1a70ce4f53b613c151b74c3f309
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-02-14 20:22:01 -0300

    ,

--- Banco de Dados/Conceito de CRUD/index.php
+++ Banco de Dados/Conceito de CRUD/index.php
@@ -1,10 +1,10 @@
 <?php
 require 'config.php';
-$lista = [];
-$sql = $pdo->query("SELECT * FROM usuarios");
-if($sql->rowCount() > 0) {
-    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
-}
+require 'dao/UsuarioDaoMysql.php';
+
+$usuarioDao = new UsuarioDaoMysql($pdo);
+$lista = $usuarioDao->findAll();
+
 ?>
 <a href="adicionar.php">Adicionar Usuário</a>
 <table border="1" width="100%">
@@ -16,9 +16,9 @@ if($sql->rowCount() > 0) {
 </tr>
 <?php foreach ($lista as $usuario):?>
     <tr>
-        <td><?php echo $usuario['id'];?></td>
-        <td><?php echo $usuario['nome'] ?></td>
-        <td><?php echo $usuario['email']?></td>
+        <td><?php $usuario->getId();?></td>
+        <td><?php $usuario->getNome(); ?></td>
+        <td><?php $usuario->getEmail();?></td>
         <td>
             <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
             <a href="excluir.php?id=<?=$usuario['id'];?>" onclick="return confirm('Deseja excluir?')">[ Excluir ]</a>
---
+++ Banco de Dados/Conceito de CRUD/models/Usuario.php
@@ -0,0 +1,34 @@
+<?php
+class Usuario {
+    private $id;
+    private $nome;
+    private $email;
+
+    public function getId() {
+        return $this->id;
+    }
+    public function setId($i) { 
+        $this->id = trim($i); //Trim remove espaços vazios que pode ocorrer
+    }
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome ($n) {
+        $this->nome = ucwords(trim($n)); //ucwords é para deixar as primeira letras maiusculas do nome
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+    public function setEmail($e) {
+        $this->email = strtolower(trim($e)); //strtolower é para deixar os textos minusculos.
+    }
+}
+
+interface UsuarioDAO {
+    public function add(Usuario $u); //Objeto da classe usuario
+    public function findAll();//Retornar uma lista de todos os usuarios
+    public function findById($id); //Retornar um ID
+    public function update(Usuario $u); // receber objeto de usuário
+    public function delete ($id); //Deletar o ID
+}
+?>
