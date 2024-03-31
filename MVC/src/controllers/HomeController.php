2599bf7037120859fd7099909f119f3b9138c82d
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-03-31 12:27:24 -0300

    .

--- MVC/src/controllers/HomeController.php
+++ MVC/src/controllers/HomeController.php
@@ -1,25 +1,37 @@
 <?php
+
 namespace src\controllers;
 
 use \core\Controller;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
 
-    public function index() {
-       $this->render('home');
+    public function index()
+    {
+        $posts = [
+            ['titulo' => 'Titulo teste 1', 'corpo' => 'Corpo de teste 1'],
+            ['titulo' => 'Titulo teste 2', 'corpo' => 'Corpo de teste 2'],
+            ['titulo' => 'Titulo teste 3', 'corpo' => 'Corpo de teste 3']
+        ];
+        $nome = 'Matheus';
+        $this->render('home', ['nome' => $nome, 'idade' => 20, 'posts' => $posts]);
     }
-    public function fotos() {
-    $this->render('fotos');
+    public function fotos()
+    {
+        $this->render('fotos');
     }
-    public function foto($parametros) {
-        echo "Acessando a foto: ".$parametros['id'];
-        
+    public function foto($parametros)
+    {
+        echo "Acessando a foto: " . $parametros['id'];
     }
-    public function sobre() {
+    public function sobre()
+    {
         $this->render('sobre');
     }
 
-    public function sobreP($args) {
-        echo 'Opa, '.$args['nome'];
+    public function sobreP($args)
+    {
+        echo 'Opa, ' . $args['nome'];
     }
 }
--- MVC/src/views/pages/home.php
+++ MVC/src/views/pages/home.php
@@ -1,2 +1,10 @@
 <?php $render('header'); ?>
-Algum conteudo
+Meu nome é: <?php echo $nome;?> <br/>
+Eu tenho: <?php echo $idade;?> anos
+<hr/>
+
+<?php 
+foreach($posts as $post): ?>
+<h3><?php echo $post ['titulo'];?></h3>
+<p><?php echo $post['corpo'];?></p>
+<?php endforeach; ?>
