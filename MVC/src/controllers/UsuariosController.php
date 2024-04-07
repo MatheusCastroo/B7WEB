21e7bd77fc2212d461518b9e8722e38f6c074637
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-04-07 11:37:21 -0300

    .

--- MVC/src/controllers/UsuariosController.php
+++ MVC/src/controllers/UsuariosController.php
@@ -4,16 +4,31 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\Usuario;
 
 class UsuariosController extends Controller
 {
 
-    public function add() {
+    public function add()
+    {
         $this->render('add');
     }
-    public function addAction() {
-        echo 'recebido';
+    public function addAction()
+    {
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email');
+        if ($name && $email) {
+            $data = Usuario::select()->where('email', $email)->execute();
+
+            if (count($data) === 0) {
+                //Insere
+                Usuario::insert([
+                    'nome' => $name,
+                    'email' => $email
+                ])->execute(); 
+                $this->redirect('/');
+            }
+        }   //redirecionar para /novo  
+        $this->redirect('/novo');
     }
-   
 }
-?>
--- MVC/src/views/pages/add.php
+++ MVC/src/views/pages/add.php
@@ -1,7 +1,12 @@
 <?php $render('header'); ?>
 
 <h2>Adicionar Novo Usuário</h2>
-<form method="POST" action="<?$base;?>/novo">
+<form method="POST" action="<? $base; ?>/novo">
+    <label for="">Nome: <br />
+        <input type="text" name="name"></label><br /><br />
+    <label for="">E-mail:<br /></label>
+    <input type="text" name="name"></label><br /><br />
+    <input type="submit" value="Adicionar">
 
 </form>
 <?php $render('footer'); ?>
