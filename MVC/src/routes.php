d9f44ea643663f05391b45ee88e613632fc9f5c1
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-04-09 07:56:20 -0300

    CRUD em MVC: Update

--- MVC/src/routes.php
+++ MVC/src/routes.php
@@ -4,7 +4,11 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/novo', 'UsuariosController@add');
 $router->post('/novo', 'UsuariosController@addAction');
+
 $router->get('/usuarios/{id}', 'UsuariosController@edit');
+$router->post('/usuarios/{id}', 'UsuariosController@editAction');
+
 $router->get('/usuario/{id}/excluir', 'UsuariosController@del');
